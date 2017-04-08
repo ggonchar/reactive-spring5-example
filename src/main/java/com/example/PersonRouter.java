fbe5c3e67fc5ad699d14f0ac3fb6f659d54ae206
Grygoriy Gonchar <grygoriy.gonchar@gmail.com>
2017-04-09 00:02:26 +0200

    Changed RequestMapping to Function for Person

---
+++ src/main/java/com/example/PersonRouter.java
@@ -0,0 +1,38 @@
+package com.example;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.RouterFunctions;
+import org.springframework.web.reactive.function.server.ServerResponse;
+import reactor.core.publisher.Mono;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON;
+import static org.springframework.web.reactive.function.server.RequestPredicates.*;
+
+@Configuration
+public class PersonRouter {
+
+    private final PersonReactiveCrudRepository repository;
+
+    public PersonRouter(PersonReactiveCrudRepository repository) {
+        this.repository = repository;
+    }
+
+    @Bean
+    public RouterFunction<?> personRoutes() {
+        return RouterFunctions
+                .route(GET("/person/{id}").and(accept(APPLICATION_JSON)), request -> {
+                    String personId = request.pathVariable("id");
+                    Mono<ServerResponse> notFound = ServerResponse.notFound().build();
+                    return repository.findOne(personId)
+                            .then(person -> ServerResponse.ok().body(Mono.just(person), Person.class))
+                            .otherwiseIfEmpty(notFound);
+                })
+                .andRoute(GET("/person").and(accept(APPLICATION_JSON)), request ->
+                        ServerResponse.ok().body(repository.findAll(), Person.class))
+                .andRoute(POST("/person").and(contentType(APPLICATION_JSON)), request ->
+                        ServerResponse.ok().body(repository.save(request.bodyToMono(Person.class)), Person.class));
+    }
+
+}
