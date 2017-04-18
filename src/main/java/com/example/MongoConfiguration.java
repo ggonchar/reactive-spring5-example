e15da696f1e22e5f467c0e5516b4bae284b5f74b
Grygoriy Gonchar <grygoriy.gonchar@gmail.com>
2017-04-19 00:21:19 +0200

    Removed MongoDB population with initial data set

--- src/main/java/com/example/MongoConfiguration.java
+++ src/main/java/com/example/MongoConfiguration.java
@@ -1,19 +1,15 @@
 package com.example;
 
 
-import com.example.person.Person;
-import com.example.person.PersonRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.mongodb.reactivestreams.client.MongoClient;
+import com.mongodb.reactivestreams.client.MongoClients;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.config.AbstractReactiveMongoConfiguration;
 import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRepositories;
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
-
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.mongodb.reactivestreams.client.MongoClient;
-import com.mongodb.reactivestreams.client.MongoClients;
 
 @Configuration
 @EnableReactiveMongoRepositories
@@ -40,13 +36,4 @@ public class MongoConfiguration extends AbstractReactiveMongoConfiguration {
         return databaseName;
     }
 
-    @Bean
-    CommandLineRunner initData(PersonRepository personRepository) {
-        return (p) -> {
-            personRepository.deleteAll().block();
-            personRepository.save(new Person("1", "Eric", "Foo")).block();
-            personRepository.save(new Person("2", "Raymond", "Bar")).block();
-            personRepository.save(new Person("3", "Paul", "Baz")).block();
-        };
-    }
 }
