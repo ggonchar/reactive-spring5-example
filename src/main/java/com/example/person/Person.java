af16484703022d0836646b719ec26137cfc2bc05
Grygoriy Gonchar <grygoriy.gonchar@gmail.com>
2017-04-18 23:26:33 +0200

    Removed setters to reduce mutability risks

--- src/main/java/com/example/person/Person.java
+++ src/main/java/com/example/person/Person.java
@@ -6,6 +6,7 @@ import org.springframework.data.mongodb.core.mapping.Document;
 @Document
 public class Person {
 
+    @Id
     private String id;
 
     private String firstname;
@@ -16,7 +17,7 @@ public class Person {
 
     private Object ipInfo;
 
-    public Person() {
+    private Person() {
     }
 
     public Person(String id, String firstname, String lastname) {
@@ -37,73 +38,24 @@ public class Person {
         return new Person(id, firstname, lastname, ip, ipInfo);
     }
 
-    @Id
     public String getId() {
         return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
     }
 
     public String getFirstname() {
         return firstname;
     }
 
-    public void setFirstname(String firstname) {
-        this.firstname = firstname;
-    }
-
     public String getLastname() {
         return lastname;
-    }
-
-    public void setLastname(String lastname) {
-        this.lastname = lastname;
     }
 
     public String getIp() {
         return ip;
     }
 
-    public void setIp(String ip) {
-        this.ip = ip;
-    }
-
     public Object getIpInfo() {
         return ipInfo;
     }
 
-    public void setIpInfo(Object ipInfo) {
-        this.ipInfo = ipInfo;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        Person person = (Person) o;
-
-        if (firstname != null ? !firstname.equals(person.firstname) : person.firstname != null) return false;
-        if (lastname != null ? !lastname.equals(person.lastname) : person.lastname != null) return false;
-        return ip != null ? ip.equals(person.ip) : person.ip == null;
-    }
-
-    @Override
-    public int hashCode() {
-        int result = firstname != null ? firstname.hashCode() : 0;
-        result = 31 * result + (lastname != null ? lastname.hashCode() : 0);
-        result = 31 * result + (ip != null ? ip.hashCode() : 0);
-        return result;
-    }
-
-    @Override
-    public String toString() {
-        return "Person{" +
-                "id='" + id + '\'' +
-                ", firstname='" + firstname + '\'' +
-                ", lastname='" + lastname + '\'' +
-                '}';
-    }
 }
