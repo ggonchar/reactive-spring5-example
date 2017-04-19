ed894f91a2cdc80b493da9d674f9f9aaf33b6bcc
Grygoriy Gonchar <grygoriy.gonchar@gmail.com>
2017-04-19 18:58:42 +0200

    Removed redundant MongoDB configuration

--- src/main/java/com/example/MongoConfiguration.java
+++ src/main/java/com/example/MongoConfiguration.java
@@ -1,30 +1,15 @@
 package com.example;
 
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.mongodb.reactivestreams.client.MongoClient;
 import com.mongodb.reactivestreams.client.MongoClients;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.config.AbstractReactiveMongoConfiguration;
 import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRepositories;
-import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 
 @Configuration
 @EnableReactiveMongoRepositories
 public class MongoConfiguration extends AbstractReactiveMongoConfiguration {
-
-    private final String databaseName;
-
-    public MongoConfiguration(@Value("${mongo.database.name}") String databaseName) {
-        this.databaseName = databaseName;
-    }
-
-    @Bean
-    ObjectMapper objectMapper() {
-        return Jackson2ObjectMapperBuilder.json().build();
-    }
 
     @Override
     public MongoClient mongoClient() {
@@ -33,7 +18,7 @@ public class MongoConfiguration extends AbstractReactiveMongoConfiguration {
 
     @Override
     protected String getDatabaseName() {
-        return databaseName;
+        return "reactive";
     }
 
 }
