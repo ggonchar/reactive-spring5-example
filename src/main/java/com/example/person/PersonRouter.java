fe2d234c19388d7fe206a07efed8e2d311908431
Grygoriy Gonchar <grygoriy.gonchar@gmail.com>
2017-04-24 20:27:26 +0200

    Added naive backpreasure example

--- src/main/java/com/example/person/PersonRouter.java
+++ src/main/java/com/example/person/PersonRouter.java
@@ -2,18 +2,23 @@ package com.example.person;
 
 import com.example.ip.IpService;
 import com.mongodb.util.JSON;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.RouterFunctions;
 import org.springframework.web.reactive.function.server.ServerResponse;
-import reactor.core.publisher.Mono;
+import reactor.core.publisher.*;
+
+import java.util.List;
 
 import static org.springframework.http.MediaType.APPLICATION_JSON;
 import static org.springframework.web.reactive.function.server.RequestPredicates.*;
 
 @Configuration
 public class PersonRouter {
+    private final Logger log = LoggerFactory.getLogger(PersonRouter.class);
 
     private final PersonRepository repository;
 
@@ -37,15 +42,38 @@ public class PersonRouter {
                 .andRoute(GET("/person").and(accept(APPLICATION_JSON)), request ->
                         ServerResponse.ok().body(repository.findAll(), Person.class))
                 .andRoute(POST("/person").and(contentType(APPLICATION_JSON)), request ->
-                        ServerResponse.ok().body(addPerson(request.bodyToMono(Person.class)), Person.class));
+                        ServerResponse.ok().body(addPerson(request.bodyToMono(Person.class)), Person.class))
+                .andRoute(GET("/person/update"), request -> {
+                        updateIpInfo();
+                        return ServerResponse.ok().build();
+                });
     }
 
     private Mono<Person> addPerson(Mono<Person> mono) {
-        return mono.flatMap(this::copyWithIpInfo).flatMap(repository::save);
+        return mono.flatMap(this::addIpInfo).flatMap(repository::save);
     }
 
-    private Mono<Person> copyWithIpInfo(Person person) {
+    private Mono<Person> addIpInfo(Person person) {
         return ipService.getIpInfo(person.getIp()).map(ipInfo -> person.copyWithIpInfo(JSON.parse(ipInfo)));
+    }
+
+    private void updateIpInfo() {
+        repository.findAll()
+                .buffer(10)
+                .onBackpressureBuffer(1000, BufferOverflowStrategy.ERROR)
+                .retry(2)
+                .parallel(2)
+                .flatMap(this::updateIpInfo)
+                .subscribe(
+                        p -> log.trace("Updated IP information for person", p),
+                        t -> log.error("Failed IP information update stream", t)
+                );
+    }
+
+    private Flux<Person> updateIpInfo(List<Person> batch) {
+        return batch.stream()
+                .map(p -> repository.save(addIpInfo(p)))
+                .reduce(Flux.empty(), (Flux<Person> previous, Flux<Person> next) -> previous.concatWith(next));
     }
 
 }
